e3470fedba7bba0d4ad2ec86f6ac3de330e71b02
Thomas Lawless <tlawless94@gmail.com>
2018-04-10 10:44:43 -0500

    set up server to serve up images

--- src/components/portfolioPieces.js
+++ src/components/portfolioPieces.js
@@ -3,28 +3,30 @@ import { Card, Image } from 'semantic-ui-react'
 
 const PortfolioPieces = () => {
     let pieces = [ {
-      imageUrl: '/src/assets/boozy.jpg',
+      imageUrl: 'assets/boozy.jpg',
       name: 'Boozy',
       description: 'A fully fledged e-commerce website. Customers can peruse and purchase wines.'
     }, {
-      imageUrl: '/src/assets/jumpman.jpg',
+      imageUrl: 'assets/jumpman.jpg',
       name: 'Jumpman',
       description: "An uncopyrighted romp through the electrician Jumpman's world"
     }, {
-      imageUrl: '/src/assets/stash.jpg',
+      imageUrl: 'assets/stash.jpg',
       name: 'Stash',
       description: 'An online inventory management system with an emphasis on a functional GUI'
     }, {
-      imageUrl: '/src/assets/thebriefing.jpg',
+      imageUrl: 'assets/thebriefing.jpg',
       name: 'The Briefing',
       description: 'A news analysis website, plug in an article and see how others are reporting on the subject.'
     }];
     return (
-      <div>
+      <div id='portfolioCardWrapper'>
+        <Card.Group>
         {pieces.map( (piece, index) => {
           return (
-            <Card key={index}>
-              <Image src= {piece.imageUrl} />
+          <div key={index} className='portfolioCard'>
+            <Card>
+              <Image fluid='true' src={piece.imageUrl} />
               <Card.Content>
                 <Card.Header>
                   {piece.name}
@@ -34,8 +36,10 @@ const PortfolioPieces = () => {
                 </Card.Description>
               </Card.Content>
             </Card>
+          </div>
           )
         })}
+        </Card.Group>
       </div>
     )
 }
