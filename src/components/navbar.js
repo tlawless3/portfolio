215644dfb2873e464ac622c7104fa24caa4530bb
Thomas Lawless <tlawless94@gmail.com>
2018-04-18 11:42:49 -0500

    navbar jumps around page

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,23 +6,30 @@ export default class Navbar extends Component {
     return (
       <div id="navbar">
         <Menu >
+          <a href="#home">
           <Menu.Item
             name='Home'
           >
             Home
           </Menu.Item>
-
+          </a>
+          
+          <a href="#portfolioCardsWrapper">
           <Menu.Item
             name='Portfolio'
           >
             Portfolio
           </Menu.Item>
-
+          </a>
+          
+          <a href="#contactInfoWrapper">
           <Menu.Item
             name='Contact'
           >
             Contact
           </Menu.Item>
+          </a>
+          
         </Menu>
       </div>
     )
