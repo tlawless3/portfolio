66d9e352ccee673b5fc7de53f3962389e5182e3a
Thomas Lawless <tlawless94@gmail.com>
2018-04-18 11:37:27 -0500

    added in navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,29 @@
 import React, {Component} from 'react'
+import { Menu } from 'semantic-ui-react'
 
 export default class Navbar extends Component {
   render () {
     return (
       <div id="navbar">
-        hello World from navbar
+        <Menu >
+          <Menu.Item
+            name='Home'
+          >
+            Home
+          </Menu.Item>
+
+          <Menu.Item
+            name='Portfolio'
+          >
+            Portfolio
+          </Menu.Item>
+
+          <Menu.Item
+            name='Contact'
+          >
+            Contact
+          </Menu.Item>
+        </Menu>
       </div>
     )
   }
